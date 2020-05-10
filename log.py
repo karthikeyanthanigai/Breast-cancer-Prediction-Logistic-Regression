0b2b27b2d85ca48bffe1a452de16cf039544800b
karthikeyan thanigaivelu <51513480+karthikeyanthanigai@users.noreply.github.com>
2020-05-10 22:45:01 +0530

    Update log.py

--- log.py
+++ log.py
@@ -3,29 +3,29 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Importing the Dataset. Here we use the dataset from ML Data.
+#Importing the Dataset
 Dataset = pd.read_csv("breast_cancer_dataset.csv")
 X = Dataset.iloc[:, :-1].values
 y = Dataset.iloc[:, [-1]].values
 
-#Now lets feature scale our independent variables from our famous scikit library
+#feature scale our independent variables 
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X = sc_X.fit_transform(X)
 
-#Splitting the dataset into training and testing and performing a good shuffle of data
+#Splitting the dataset into training and testing set
 from sklearn.model_selection import train_test_split
 X_train , X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=32, shuffle=True)
 
-#Now lets define our Logistic Regression model. 
+#Logistic Regression model. 
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(max_iter=1000, random_state = 32, multi_class='ovr', solver='liblinear')
 classifier.fit(X_train, y_train)
 
-#Now lets predict with our test set
+#predict our test set
 y_pred = classifier.predict(X_test)
 
-#Its time for evaluating our model.
+#evaluating our model.
 from sklearn.metrics import confusion_matrix, accuracy_score, f1_score
 acc_train = accuracy_score(y_train, classifier.predict(X_train))
 f1_train = f1_score(y_train, classifier.predict(X_train), average= 'weighted')
@@ -34,7 +34,7 @@ print("Traing set results")
 print("ACCURACY for train set",acc_train)
 print("F1 SCORE for train set",f1_train)
 
-#Now lets see how well is our model. So now lets evaluate with our test set
+#evaluate our test set
 acc_test = accuracy_score(y_test, y_pred)
 f1_test = f1_score(y_test, y_pred, average= 'weighted')
 
@@ -42,6 +42,6 @@ print("Test set results")
 print("ACCURACY for test set",acc_test)
 print("F1 SCORE for test set",f1_test)
 
-#Now lets have our famous Confusion Matrix to visually understand.
+#Confusion Matrix 
 cm = confusion_matrix(y_test,y_pred)
 print(cm)
